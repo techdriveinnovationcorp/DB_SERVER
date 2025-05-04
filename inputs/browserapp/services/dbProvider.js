8ef30de534f1ea3e3ee3a267996baa55fd578589
techdriveinnovationcorp <techdriveinnovationcorp@gmail.com>
2025-05-04 15:26:56 +0530

    One to one relationship fetch introduced

--- inputs/browserapp/services/dbProvider.js
+++ inputs/browserapp/services/dbProvider.js
@@ -60,6 +60,74 @@ const dbProvider = {
     }
 
     listenerService.notifyListeners(listenerName, {records: [], status: 'completed'});
+  },
+
+  async listFetch({objectHierarchy}, listenerName) {
+
+    const getChildByType = (children, type) => _.filter(children, { objectType: type });
+
+    const fetchOneToOneChildren = async (oneToOneArray, parentRec) => {
+      return Promise.all(oneToOneArray.map(async ({ objectName, fieldName, childObject, rootPath }) => {
+        const refId = parentRec?.id?.split('_2_')[1];
+        if (!refId) return;
+
+        const query = {
+          $and: [
+            { field: 'type', operator: '==', value: objectName },
+            { field: fieldName, operator: '==', value: refId }
+          ],
+          $includeFields: true,
+        };
+
+        const res = await User.searchDoc(query);
+        const childRec = res?.docs?.[0];
+        if (childRec) {
+          listenerService.notifyListeners(listenerName, { records: childRec, rootPath: rootPath, status: 'inprogress' });
+          return handleResponse(childObject, childRec);
+        }
+      }));
+    };
+
+    const fetchHeaderChildren = async (headerArray, parentRec) => {
+      return Promise.all(headerArray.map(async ({ objectName, fieldName, childObject }) => {
+        const headerId = `${objectName}_2_${parentRec[fieldName]}`;
+        const res = await User.allDocById(headerId);
+        if (res?.id) {
+          listenerService.notifyListeners(listenerName, { records: res, rootPath: childObject['rootPath'], status: 'inprogress' });
+          return handleResponse(childObject, res);
+        }
+      }));
+    };
+
+    const handleResponse = async (childObject, parentRec) => {
+      if (!childObject?.length) return;
+
+      const oneToOneArray = getChildByType(childObject, 'one_to_one');
+      const headerArray = getChildByType(childObject, 'header');
+
+      await Promise.all([
+        fetchOneToOneChildren(oneToOneArray, parentRec),
+        fetchHeaderChildren(headerArray, parentRec)
+      ]);
+    };
+
+    const query = {
+      $and: [
+        { field: 'type', operator: '==', value: objectHierarchy['objectName'] }
+      ],
+      $sort: { "field": "createdon", "direction": "asc" },
+      $includeFields: objectHierarchy['includeFields'],
+    };
+
+    const primaryRes = await User.searchDoc(query);
+    if (primaryRes?.['docs'] && primaryRes['docs'].length > 0) {
+      for (const element of primaryRes['docs']) {
+        listenerService.notifyListeners(listenerName, { records: element, rootPath: objectHierarchy['rootPath'], status: 'inprogress' });
+        await handleResponse(objectHierarchy.childObject, element);
+      }
+    }
+
+    listenerService.notifyListeners(listenerName, {records: {}, status: 'completed'});
   }
 };
 
